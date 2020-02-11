7940dd9d5024ba7469253cca3da83a533ffd1e23
Leonardo Cardozo Ferreira <39681326+leonardo329cf@users.noreply.github.com>
2020-02-11 15:45:01 -0300

    Revert "Revert "Security implemented and tests adapted""

--- src/main/java/com/leonardocardozo/notesappbackend/services/ContributionService.java
+++ src/main/java/com/leonardocardozo/notesappbackend/services/ContributionService.java
@@ -2,6 +2,7 @@ package com.leonardocardozo.notesappbackend.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -120,6 +121,15 @@ public class ContributionService {
 		}
 		else {
 			throw new ResourceNotFoundException(noteId + " and username, " + username);
+		}
+	}
+	
+	public boolean isContributor(String username, Long noteId, Integer permission) {
+		Contribution cont = contRepo.findByUsernameAndNoteId(username, noteId);
+		if(cont != null && cont.getPermission() >= permission) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 
