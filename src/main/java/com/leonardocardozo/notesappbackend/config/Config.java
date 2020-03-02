200c53d1d90fd1c0555231f938e93b1424abe606
leonardo329cf <leonardo329cf@gmail.com>
2020-03-02 19:50:34 -0300

    Swagger 2 implemented

--- src/main/java/com/leonardocardozo/notesappbackend/config/Config.java
+++ src/main/java/com/leonardocardozo/notesappbackend/config/Config.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -16,9 +17,25 @@ import com.leonardocardozo.notesappbackend.repositories.ContributorRepository;
 import com.leonardocardozo.notesappbackend.repositories.NoteRepository;
 import com.leonardocardozo.notesappbackend.repositories.UserRepository;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @Configuration
 @Profile("test")
 public class Config implements CommandLineRunner{
+	
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
 
 	@Autowired
 	private UserRepository userRepo;
