0ed6490ac0e819d1354cb9e0ac7d71bddce04855
leonardo329cf <leonardo329cf@gmail.com>
2020-03-06 16:06:27 -0300

    Enabling and adding @PreAuthorize, blocking access for non admin users on findAll notes

--- src/main/java/com/leonardocardozo/notesappbackend/controllers/NoteController.java
+++ src/main/java/com/leonardocardozo/notesappbackend/controllers/NoteController.java
@@ -10,6 +10,7 @@ import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -38,6 +39,7 @@ public class NoteController implements Serializable {
 	@Autowired
 	private ContributionService contService;
 
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@GetMapping
 	public ResponseEntity<List<NoteUtil>> findAll() {
 		var resp = noteService.findAll();
